67a25b8392a2b4834a74d0eeee183c1012c8651c
Nik <61439310+NikV2@users.noreply.github.com>
2020-07-15 02:41:25 +0300

    Commit

--- src/main/java/me/nik/cardinal/api/CardinalAPI.java
+++ src/main/java/me/nik/cardinal/api/CardinalAPI.java
@@ -4,9 +4,26 @@ import org.bukkit.entity.Player;
 
 public interface CardinalAPI {
 
+    /**
+     * Gets a player's Ping
+     *
+     * @param player The player
+     * @return The player's ping
+     */
     int getPing(Player player);
 
+    /**
+     * Gets the Server's last TPS
+     *
+     * @return The server's last TPS
+     */
     double getTPS();
 
+    /**
+     * Check if a player is an admin
+     *
+     * @param player The player to check
+     * @return Whether or not that player is an Admin
+     */
     boolean isAdmin(Player player);
 }
