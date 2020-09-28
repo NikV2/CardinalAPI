174be92cd8d7f34351804981ca47fd52a781c944
Nik <61439310+NikV2@users.noreply.github.com>
2020-09-28 16:32:16 +0300

    Commit

--- src/main/java/me/nik/cardinal/api/events/PlayerPunishEvent.java
+++ src/main/java/me/nik/cardinal/api/events/PlayerPunishEvent.java
@@ -2,17 +2,15 @@ package me.nik.cardinal.api.events;
 
 import me.nik.cardinal.api.enums.CheckType;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
-public class PlayerPunishEvent extends Event implements Cancellable {
+public class PlayerPunishEvent extends Event {
 
     private static final HandlerList handlers = new HandlerList();
 
     private final Player player;
     private final CheckType checkType;
-    private boolean cancel = false;
 
     public PlayerPunishEvent(Player player, CheckType checkType) {
         this.player = player;
@@ -33,13 +31,5 @@ public class PlayerPunishEvent extends Event implements Cancellable {
 
     public HandlerList getHandlers() {
         return handlers;
-    }
-
-    public boolean isCancelled() {
-        return this.cancel;
-    }
-
-    public void setCancelled(boolean cancel) {
-        this.cancel = cancel;
     }
 }
